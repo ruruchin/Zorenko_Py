199fbc5a688ea660d7fce873d6b1101b175475a7
ruruchin <konstantinzorenko3@gmail.com>
2023-10-10 18:40:16 +0300

    йqй

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,8 +1,8 @@
 #Дано двузначное число. Найти сумму и произведение его цифр.
-chislo=int(input("Введите двузначное число: ", sep='|',  end='|'))
+chislo=int(input("Введите двузначное число: "))
 chislo_1 = chislo % 10
 chislo_2 = chislo // 10
 slochenie = chislo_1 + chislo_2
 proizvedenie = chislo_1 * chislo_2
 print("Результат суммы цифр числа:",slochenie, sep=' ')
-print("Результат произведен цифр числа:",proizvedenie, sep=' ')
+print("Результат произведения цифр числа:",proizvedenie, sep=' ')
