35e14fd30b2586233c4f579baf5426e992f9e703
ruruchin <145475049+ruruchin@users.noreply.github.com>
2024-02-17 00:17:44 +0300

    PZ_12.py

---
+++ PZ_12.py
@@ -0,0 +1,20 @@
+
+from random import randrange, random, randint
+tripletick = [randint(0, 1000) for xxx in range(100) if xxx > 0]
+for xx in range(len(tripletick)):
+    xx = xx + 1
+print(tripletick)
+print(f'НУ НОРМАЛЬНЫЕ ЧИСЛА -> {xx}')
+
+
+
+
+
+lsd=['Валентин', 'Петр', 'Анна', 'Евгений', 'Константин', 'Валерия', 'Юлия']
+new = [name for name in lsd if len(name) <=5]
+print(new)
+
+
+
+
+
